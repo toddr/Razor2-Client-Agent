6244ee8be4a3504ac56ed4f9fd60f4ac15bc3e11
Todd Rinaldo <toddr@cpanel.net>
2019-06-03 17:51:28 -0500

    Bump to 2.86 for release to CPAN

--- lib/Razor2/Preproc/deHTMLxs.pm
+++ lib/Razor2/Preproc/deHTMLxs.pm
@@ -12,7 +12,7 @@ our @ISA = qw(Exporter);
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
 our @EXPORT  = qw();
-our $VERSION = '2.85';
+our $VERSION = '2.86';
 
 XSLoader::load( 'Razor2::Preproc::deHTMLxs', $VERSION );
 
